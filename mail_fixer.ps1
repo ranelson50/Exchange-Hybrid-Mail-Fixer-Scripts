3ebbe3cb7a830487cccafd354877851b9a2b9f07
ranelson50 <53538467+ranelson50@users.noreply.github.com>
2020-11-12 08:29:03 -0500

    Add files via upload

---
+++ mail_fixer.ps1
@@ -0,0 +1,35 @@
+﻿echo "
+
+
+Hey, I am a robot and I am going to help you fix this person's mailbox.
+
+
+"
+echo "
+
+
+Feed me some credentials
+
+
+"
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange `
+-ConnectionUri https://outlook.office365.com/powershell-liveid/ `
+-Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -DisableNameChecking
+
+$MailUser = Read-Host -Prompt "
+
+
+What is the mailbox user's name? (first name.last name typically)
+
+
+"
+
+Get-Mailbox $MailUser | Format-List ExchangeGuid
+
+echo "Copy that string, you will need it in a minute"
+
+Remove-PSSession $Session
+
+powershell.exe -noexit -command ". 'D:\Program Files\Microsoft\Exchange Server\V15\bin\RemoteExchange.ps1'; Connect-ExchangeServer -auto -ClientApplication:ManagementShell; . 'C:\mail_fixer_2.ps1'"
---
+++ mail_fixer_2.ps1
@@ -0,0 +1,33 @@
+﻿$MailUser = Read-Host -Prompt "
+
+
+My robot memory is bad. What is that user's name again? (Again, first name.last name)
+
+
+"
+
+$GUID = Read-Host -Prompt '
+
+
+What was that GUID again?
+
+
+'
+
+$RemoteAddress = "$MailUser@contoso.onmicrosoft.com"
+
+$PrimarySMTP = "$MailUser@contoso.com"
+
+Enable-RemoteMailbox "$MailUser" -RemoteRoutingAddress "$RemoteAddress" -PrimarySmtpAddress "$PrimarySMTP"
+
+Set-RemoteMailbox "$MailUser" -ExchangeGuid "$GUID"
+
+Get-AdUser "$MailUser" | Set-AdObject -Replace @{msExchRecipientDisplayType=-1073741818}
+
+echo "
+
+
+If you have made it this far without error then you should be okay.
+
+
+"
